0774af4f4981af46b8eaf0f8b94f68b6ece6a710
Charles Siffrein-Blanc <chsiffre@z4r12p5.42lyon.fr>
2024-05-21 16:50:13 +0200

    awaiting Henrik

--- sources/commands/unban.cpp
+++ sources/commands/unban.cpp
@@ -2,7 +2,6 @@
 
 void Command::unban(std::vector<std::string> args, Client* client)
 {
-    std::cout << "UNBAN CMD\t" << args[0] << client->get_nickname() << std::endl;
 	if (client->get_isConnected() == false)
 		return;
     if (args.size() < 3)
@@ -22,7 +21,6 @@ void Command::unban(std::vector<std::string> args, Client* client)
 	{
         if (it->second->is_banned(iter->second->get_nickname()) && iter->first != client->get_fd())
         {
-            std::cout << "debug in the if to deban " << std::endl;
             it->second->unban_client(iter->second);
             return ;
         }
