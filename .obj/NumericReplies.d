c314c850f00862ad0ddce6524e8ec75360da0dfc
Henrik <henriksilverbage@gmail.com>
2024-05-30 16:04:23 +0200

    ok

---
+++ .obj/NumericReplies.d
@@ -0,0 +1,11 @@
+.obj/NumericReplies.o: sources//NumericReplies.cpp \
+ includes/NumericReplies.hpp includes/Server.hpp includes/Client.hpp \
+ includes/Channel.hpp includes/Client.hpp includes/Command.hpp \
+ includes/NumericReplies.hpp
+includes/NumericReplies.hpp:
+includes/Server.hpp:
+includes/Client.hpp:
+includes/Channel.hpp:
+includes/Client.hpp:
+includes/Command.hpp:
+includes/NumericReplies.hpp:
